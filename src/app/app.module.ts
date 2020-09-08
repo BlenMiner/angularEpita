16ab1f1092cf2c9acd3f31f1d2d91ebd877ad87d
Valentin BORDEIANU <blenminer123@gmail.com>
2020-09-08 14:40:00 +0200

    playground

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { VBU4Component } from './vbu4/vbu4.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/vbu4/vbu4.component.ts
+++ src/app/vbu4/vbu4.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-vbu4',
+  selector: 'fun',
   templateUrl: './vbu4.component.html',
   styleUrls: ['./vbu4.component.css']
 })
 export class VBU4Component implements OnInit {
 
+  @Input() message: string;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  pp(msg: string): string{
+    let res: string = "";
+    for (let i = 0; i < msg.length; ++i) {
+      if (i % 2 == 0)
+           res += msg.charAt(i).toLowerCase();
+      else res += msg.charAt(i).toUpperCase();
+    }
+    return res;
   }
 
 }
